b3f9da4b6d866bf2ccf06855689513050c9ef02e
javaheri2016 <Marker22%>
2020-08-02 23:56:23 +0200

    test api

---
+++ test.py
@@ -0,0 +1,12 @@
+from requests import api
+import time
+
+r = api.get("https://javaheri.pl")
+time.sleep(5)
+print(r.status_code)
+time.sleep(5)
+print(r.headers['content-type'])
+time.sleep(5)
+print(r.encoding)
+time.sleep(5)gi
+print(r.json)
