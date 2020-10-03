dbd6936b544e89557b7547d3cab66a57efae220c
javaheri2016 <Marker22%>
2020-10-03 23:48:59 +0200

    drag and drop fix

--- homeworks/simple_test_site/tests/page_objects/drag_and_drop_page.py
+++ homeworks/simple_test_site/tests/page_objects/drag_and_drop_page.py
@@ -24,7 +24,7 @@ def drag_and_drop_content_visible(driver_instance):
 
 def check_drag_and_drop(driver_instance):
     driver_instance.implicitly_wait(10)
-    driver_instance.get('http://the-internet.herokuapp.com/drag_and_drop')
+    driver_instance.get('http://simpletestsite.fabrykatestow.pl/')
 
     with open(os.path.abspath('drag_and_drop_helper.js'), 'r') as js_file:
         line = js_file.readline()
@@ -33,6 +33,6 @@ def check_drag_and_drop(driver_instance):
             script += line
             line = js_file.readline()
 
-    driver_instance.execute_script(script + "$('#column-a').simulateDragDrop({ dropTarget: '#column-b'});")
+    driver_instance.execute_script(script + "jQuery('#column-a').simulateDragDrop({ dropTarget: '#column-b'});")
     sleep(2)
     return True
